191adef4c5ecbfc0248a5ffc6af2de224d523f57
Jeremiah Wuenschel <jeremiah.wuenschel@gmail.com>
2013-09-27 22:14:55 -0700

    Added internal pull-up for the button pin

--- button/button.ino
+++ button/button.ino
@@ -4,11 +4,12 @@ int button = A5;
 void setup() {
   pinMode( LED, OUTPUT );
   pinMode( button, INPUT );
+  digitalWrite( button, HIGH ); // enable internal pull-up
 }
 
 void loop() {
   int pressed = digitalRead( button );
-  if ( pressed == HIGH ) {
+  if ( pressed == LOW ) {
     digitalWrite( LED, HIGH );
   } else {
     digitalWrite( LED, LOW );
